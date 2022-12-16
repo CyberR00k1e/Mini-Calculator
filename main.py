dbbe291d048c78762f47e2353230d3b373b69dbf
srt <sharat.nautiyal@gmail.com>
2022-12-16 19:41:59 +0800

    Added function.py file and button

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import json
-
-#a=input("enter your answer")
-with open("Questions.json", "r") as file:
-    content = file.read()
-
-data =  json.loads(content)
-score=0
-for i in data:
-    print(i["text"])
-    for a,b in enumerate(i["options"]):
-        print(f"{a+1}.{b}")
-    ans=input("your answer")
-    if ans==i["correct_answer"]:
-        score=score+1
-        print("correct answers!!")
-
-print("-------"+"\n")
-print(f"your total score is {score}")
+import PySimpleGUI as sg
 
 
-
+window = sg.Window("POC Tracking App", layout=[""])
+button=sg.Button("Add")
+text=sg.InputText()
+window.read()
+window.close()
 
