a12b8a0d21d0c416d1bc3f7972aae2cf7ac8081c
srt <sharat.nautiyal@gmail.com>
2022-12-17 18:48:28 +0800

    Added calculator function and output

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ label2=sg.Text(key="output")
 window = sg.Window("Calculator", layout=[[button],[label],[input],[label2]])
 
 
-
-
-
-
 while True:
     event, value = window.read()
     r = function.test(int(value["inputs"][0]), int(value["inputs"][2]))
