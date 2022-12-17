0cf767d167a84ca8436a10fe4e6cfa7408a2dd7f
srt <sharat.nautiyal@gmail.com>
2022-12-17 18:47:24 +0800

    Added calculator function and output

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import PySimpleGUI as sg
 import function
 
 
-window = sg.Window("POC Tracking App", layout=[""])
 button=sg.Button("Add")
-text=sg.InputText()
-window.read()
+label=sg.InputText("Please provide 2 numbers below with space between them", key="number")
+input=sg.InputText(key="inputs")
+label2=sg.Text(key="output")
+window = sg.Window("Calculator", layout=[[button],[label],[input],[label2]])
+
+
+
+
+
+
+while True:
+    event, value = window.read()
+    r = function.test(int(value["inputs"][0]), int(value["inputs"][2]))
+    match event:
+            case Add:
+                window["output"].update(value=r)
+
+
 window.close()
 
